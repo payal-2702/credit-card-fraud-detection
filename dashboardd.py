35265402c712c4c81b171e95cebb9163cb7a77ff
Riya <005riyariya@gmail.com>
2025-04-17 18:40:13 +0530

    add requirements

--- dashboardd.py
+++ dashboardd.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title ("credit card fraud detection")
+st.write ("Enter following details")
 amount=st.number_input("Transaction Amount")
 card= st.selectbox("Card Type",["Visa","Master card","other"]) # type: ignore
 if st.button("Submit"):
-    st.write("Transaction Submitted!")
-
+   st.write("Transaction Submitted!")
