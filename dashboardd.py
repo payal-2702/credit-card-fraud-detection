abe968d7bc269ff92b89b5ce82d09b92eef442e9
Riya <005riyariya@gmail.com>
2025-04-17 19:04:46 +0530

    DASHBOARD

--- dashboardd.py
+++ dashboardd.py
@@ -4,4 +4,4 @@ st.write ("Enter following details")
 amount=st.number_input("Transaction Amount")
 card= st.selectbox("Card Type",["Visa","Master card","other"]) # type: ignore
 if st.button("Submit"):
-   st.write("Transaction Submitted!")
+   st.write("TRANSACTION SUBMITTED")
