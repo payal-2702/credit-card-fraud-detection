d9554306bf515298a6a404bc0f9c67de24d9fdc3
Riya <005riyariya@gmail.com>
2025-04-17 20:23:40 +0530

    changed

--- dashboardd.py
+++ dashboardd.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import streamlit as st
 st.title ("credit card fraud detection")
 st.write ("Enter following details")
@@ -6,6 +6,3 @@ amount=st.number_input("Transaction Amount")
 card= st.selectbox("Card Type",["Visa","Master card","other"]) 
 if st.button("Submit"):
    st.write("TRANSACTION SUBMITTED")
-=======
-
->>>>>>> b5c7dfd9a368c82ad706e2ad508b210f248d1178
